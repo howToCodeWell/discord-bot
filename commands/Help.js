78e7730ca9c8997527cb1ea78255488c17141932
peterfisher <c3T-y7J-vo3-qpg>
2019-10-01 16:16:36 +0100

    Updating help

--- commands/Help.js
+++ commands/Help.js
@@ -7,7 +7,10 @@ class Help {
         + "--roll : Roll a dice \n"
         + "--ping : Play ping pong \n"
         + "--lights : Coming Soon \n"
-        + "--schedule : Get the schedule \n"
+        + "--schedule : Get the schedule \n \n"
+        + "Want to improve this bot? \n "
+        + "Send a PR to https://github.com/howToCodeWell/discord-bot \n "
+
         + "```\n";
 
         bot.sendMessage({to: channelID, message: message});
